3f53088e2a76c736401b59d10717d1885f767248
madhavan.k <madhavan.k@MYSHEC496992L.ad.infosys.com>
2023-09-18 15:09:26 +0100

    Uploading all files First Commit

---
+++ Ref/src/myPack/Ref.java
@@ -0,0 +1,20 @@
+package myPack;
+import java.util.Arrays;
+
+class Ref {
+
+	
+	
+	public static void main(String[] args) {
+		int a = 200;
+		byte b = (byte)a;
+		float f = 5.6f;
+		int c = (int)f;
+		
+		System.out.print(c);
+
+        
+	}
+
+}
+	
